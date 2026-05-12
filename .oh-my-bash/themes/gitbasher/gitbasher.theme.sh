771905fa9a39ce9139a1b8960b7f8b6e713fcbbb
Leona Araujo <araujo.contato@gmail.com>
2026-05-12 14:24:07 -0300

    feat: clean gitbasher theme (speed up)

--- .oh-my-bash/themes/gitbasher/gitbasher.theme.sh
+++ .oh-my-bash/themes/gitbasher/gitbasher.theme.sh
@@ -18,45 +18,6 @@ prompt_git() {
   fi
 }
 
-has_cmd() {
-  [[ -n $(command -v $1 2>/dev/null) ]]
-}
-
-prompt_node() {
-  node_exec="node"
-
-  if [[ "$OSTYPE" == "cygwin" ]]; then
-    node_exec="node.exe"
-  elif [[ "$OSTYPE" == "msys" ]]; then
-    node_exec="node.exe"
-  elif [[ "$OSTYPE" == "win32" ]]; then
-    node_exec="node.exe"
-  else
-    node_exec="node"
-  fi
-
-  versions=""
-
-  has_cmd $node_exec && \
-    versions="node $($node_exec -v) | "
-
-  has_cmd npm && {
-    versions=${versions::${#versions}-3}
-    versions="${versions} (npm $(npm -v)) | "
-  }
-
-  has_cmd python && \
-    versions="${versions}python $(python --version | awk '{print $2}') | "
-
-  # has_cmd terraform && \
-  #   versions="${versions}terraform $(terraform --version | head -n 1 | awk '{print $2}') | "
-
-  # Cut the last three character " | "
-  versions=${versions::${#versions}-3}
-
-  echo -e "${versions}"
-}
-
 prompt_git_dirty() {
   dirty=$(git status -s 2>/dev/null | tail -n 1)
   [[ -n $dirty ]] && echo "$LIGHT_GREEN$1 *"
@@ -69,7 +30,7 @@ prompt_error() {
 
 build_prompt() {
   ERROR_NUM=$?
-  PS1="$GREEN\u@\h $MAGENTA$(prompt_sys_name) $YELLOW\w $(prompt_git_dirty $(prompt_git)) $RED$(prompt_error $ERROR_NUM)\n$DEFAULT$(prompt_node)$DEFAULT\n$ "
+  PS1="$GREEN\u@\h $MAGENTA$(prompt_sys_name) $YELLOW\w $(prompt_git_dirty $(prompt_git)) $RED$(prompt_error $ERROR_NUM)$DEFAULT\n$ "
 }
 
 PROMPT_COMMAND=build_prompt
